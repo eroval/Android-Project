f27836b02753a1fdf7461d60ad38f793083162f7
Denis Zangarov <denisimo_98@yahoo.com>
2022-12-03 15:38:10 +0200

    fragments added

--- CalisthenicsApp/app/src/main/java/com/example/f95108_calisthenics_app/MainActivity.java
+++ CalisthenicsApp/app/src/main/java/com/example/f95108_calisthenics_app/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.f95108_calisthenics_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private Button bodyBtn = findViewById(R.id.btnBody);
+    private Button activityBtn = findViewById(R.id.btnActivity);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bodyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                fragmentManager.beginTransaction();
+            }
+        });
+
+
     }
 }
