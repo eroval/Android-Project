c433965794a807b464049b8c5a4a50f42a352c78
Denis Zangarov <denisimo_98@yahoo.com>
2022-12-04 17:41:11 +0200

    initial state

--- CalisthenicsApp/app/src/main/java/com/example/f95108_calisthenics_app/MainActivity.java
+++ CalisthenicsApp/app/src/main/java/com/example/f95108_calisthenics_app/MainActivity.java
@@ -6,25 +6,28 @@ import androidx.fragment.app.FragmentManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
 
 
-    private Button bodyBtn = findViewById(R.id.btnBody);
-    private Button activityBtn = findViewById(R.id.btnActivity);
+    private Button bodyBtn;
+    private Button activityBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        bodyBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                FragmentManager fragmentManager = getSupportFragmentManager();
-                fragmentManager.beginTransaction();
-            }
-        });
-
+        bodyBtn = findViewById(R.id.btnBody);
+        activityBtn = findViewById(R.id.btnActivity);
+//        bodyBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                FragmentManager fragmentManager = getSupportFragmentManager();
+//                fragmentManager.beginTransaction();
+//            }
+//        });
 
     }
 }
