978ddaf167c0dc1d2d8372fbad8b8a91e726661f
Denis Zangarov <denisimo_98@yahoo.com>
2023-01-05 11:26:10 +0200

    updated with service

--- CalisthenicsApp/app/src/main/java/com/example/f95108_calisthenics_app/MainActivity.java
+++ CalisthenicsApp/app/src/main/java/com/example/f95108_calisthenics_app/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -46,6 +47,9 @@ public class MainActivity extends AppCompatActivity {
         activityBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MyDatabaseService.class);
+                startService(intent);
+                stopService(intent);
                 setFragment("activities");
             }
         });
